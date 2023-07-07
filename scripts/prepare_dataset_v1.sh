2111af75456dd18b1031c9a3878e68f0092717fa
Zenon Lamprou <wrb15144@tehr09.cis.strath.ac.uk>
2023-07-07 11:11:06 +0100

    Merging

--- scripts/prepare_dataset_v1.sh
+++ scripts/prepare_dataset_v1.sh
@@ -3,4 +3,4 @@ echo "This script also generates tenary sentiment_labels.json file for ZuCo task
 echo "Note: the sentences in ZuCo task1-SR do not overlap with sentences in filtered StanfordSentimentTreebank "
 echo "Note: This process can take time, please be patient..."
 
-python3 ./util/construct_dataset_mat_to_pickle_v1.py -t task1-SR -v 2 -d "vol/bulkdata/wrb15144/encoder_decoder/osfstorage-archive/" -o "vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task1"
+python3 ../util/construct_dataset_mat_to_pickle_v1.py -t task1-SR -v 2 -d "vol/bulkdata/wrb15144/encoder_decoder/osfstorage-archive/" -o "vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task1"
