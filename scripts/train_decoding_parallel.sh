242a76db48bb2500e287fb66cc032da0f9fbf57f
Zenon Lamprou <wrb15144@tehr09.cis.strath.ac.uk>
2023-07-25 17:47:22 +0100

    Bug fixing

--- scripts/train_decoding_parallel.sh
+++ scripts/train_decoding_parallel.sh
@@ -1,4 +1,4 @@
-accelerate launch ../train_decoding.py --model_name BrainTranslator \
+accelerate launch ../train_decoding_parallel.py --model_name BrainTranslator \
     --task_name task1_task2_task3 \
     --one_step \
     --pretrained \
@@ -7,7 +7,7 @@ accelerate launch ../train_decoding.py --model_name BrainTranslator \
     --num_epoch_step2 30 \
     -lr1 0.00005 \
     -lr2 0.0000005 \
-    -b 32 \
+    -b 16 \
     -cuda cuda:0 \
     --dataset_path_task1 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task1/task1-SR-dataset.json \
     --dataset_path_task2 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task2/task2-NR-dataset.json \
