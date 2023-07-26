8d648e35cfb82e417c189bc93791f0cd17b3146f
zenon009 <zenon.lambrou@gmail.com>
2023-07-26 11:18:21 +0300

    Bug fixing

--- scripts/train_decoding_parallel.sh
+++ scripts/train_decoding_parallel.sh
@@ -8,7 +8,7 @@ accelerate launch ../train_decoding_parallel.py --model_name BrainTranslator \
     -lr1 0.00005 \
     -lr2 0.0000005 \
     -b 16 \
-    -cuda cuda:0 \
+    -cuda cuda \
     --dataset_path_task1 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task1/task1-SR-dataset.json \
     --dataset_path_task2 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task2/task2-NR-dataset.json \
     --dataset_path_task3 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task3/task3-TSR-dataset.json \
