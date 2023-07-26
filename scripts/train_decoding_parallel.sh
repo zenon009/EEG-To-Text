9637333fbe407fa5e0414f2cd67b5581880bca71
Zenon Lamprou <wrb15144@tehr09.cis.strath.ac.uk>
2023-07-26 12:34:53 +0100

    Merging

--- scripts/train_decoding_parallel.sh
+++ scripts/train_decoding_parallel.sh
@@ -1,4 +1,4 @@
-accelerate launch ../train_decoding_parallel.py --model_name BrainTranslator \
+accelerate launch --multi_gpu --num_processes 2 ../train_decoding_parallel.py --model_name BrainTranslator \
     --task_name task2 \
     --one_step \
     --pretrained \
