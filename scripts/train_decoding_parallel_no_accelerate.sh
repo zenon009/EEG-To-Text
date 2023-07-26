ba58a11298e71ff9a8f4fe82f92d4f90e234d43d
Zenon Lamprou <wrb15144@tehr09.cis.strath.ac.uk>
2023-07-26 12:40:30 +0100

    Merging

---
+++ scripts/train_decoding_parallel_no_accelerate.sh
@@ -0,0 +1,17 @@
+python3 ../train_decoding_parallel.py --model_name BrainTranslator \
+    --task_name task2 \
+    --one_step \
+    --pretrained \
+    --not_load_step1_checkpoint \
+    --num_epoch_step1 5 \
+    --num_epoch_step2 5 \
+    -lr1 0.00005 \
+    -lr2 0.0000005 \
+    -b 8 \
+    -cuda cuda \
+    --dataset_path_task1 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task1/task1-SR-dataset.json \
+    --dataset_path_task2 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task2/task2-NR-dataset.json \
+    --dataset_path_task3 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task3/task3-TSR-dataset.json \
+    -s "./"
+
+
