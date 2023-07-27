959c037d133309e0dae5bc52af428f3914299f99
zenon009 <zenon.lambrou@gmail.com>
2023-07-27 10:07:14 +0300

    Bug fixing

---
+++ scripts/train_decoding_distributed.sh
@@ -0,0 +1,17 @@
+python -m torch.distributed.launch ../train_decoding.py --model_name BrainTranslator \
+    --task_name task1_task2_task3 \
+    --one_step \
+    --pretrained \
+    --not_load_step1_checkpoint \
+    --num_epoch_step1 20 \
+    --num_epoch_step2 10 \
+    -lr1 0.00005 \
+    -lr2 0.0000005 \
+    -b 16 \
+    -cuda cuda \
+    --dataset_path_task1 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task1/task1-SR-dataset.json \
+    --dataset_path_task2 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task2/task2-NR-dataset.json \
+    --dataset_path_task3 /vol/bulkdata/wrb15144/encoder_decoder/preprocessed_zuco_1/task3/task3-TSR-dataset.json \
+    -s "./"
+
+
